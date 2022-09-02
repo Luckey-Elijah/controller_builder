13329dc05869014e1d2d9d1fff0470a54b543a3c
Elijah Luckey <luckeyelijah112@gmail.com>
2022-09-02 09:24:01 -0400

    clean up example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,16 +1,23 @@
-import 'dart:developer';
-
 import 'package:controller_builder/controller_builder.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: ControllerBuilderExample());
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: ControllerBuilderExample(),
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -19,29 +26,17 @@ class ControllerBuilderExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ControllerBuilder<TextEditingController>(
-            create: () {
-              final controller = TextEditingController(text: 'Hello, world!');
-              return controller..addListener(someListener);
-            },
-            builder: (context, controller) {
-              return TextField(
-                controller: controller,
-                onChanged: log,
-              );
-            },
-            dispose: (controller) {
-              controller
-                ..removeListener(someListener)
-                ..dispose();
-            },
-          ),
-        ),
-      ),
+    return ControllerBuilder<TextEditingController>(
+      create: () {
+        final controller = TextEditingController(text: 'Hello, world!');
+        return controller..addListener(someListener);
+      },
+      builder: (context, controller) => TextField(controller: controller),
+      dispose: (controller) {
+        controller
+          ..removeListener(someListener)
+          ..dispose();
+      },
     );
   }
 }
